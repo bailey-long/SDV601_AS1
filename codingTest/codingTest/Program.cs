6e820b5534bd4a4f4c7d4f5595bf327a090bb5b1
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-02-28 17:44:23 +1300

    GST calculator

--- codingTest/codingTest/Program.cs
+++ codingTest/codingTest/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void calculateGst(decimal price)
+{
+    Console.WriteLine("price with gst " + price + "$");
+    Console.WriteLine("gst difference " + Math.Round(price * 0.15m, 2) + "$");
+    Console.WriteLine("Price without gst " +  Math.Round(price * 0.85m, 2) + "$");
+};
+
+calculateGst(11.50m);
+
