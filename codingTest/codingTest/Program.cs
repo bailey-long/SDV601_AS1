5506c8163ebf176a0c276a6e544c92483a75fe75
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-02-28 18:02:28 +1300

    Updated calculator to take user input

--- codingTest/codingTest/Program.cs
+++ codingTest/codingTest/Program.cs
@@ -1,10 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-void calculateGst(decimal price)
+void calculateGst()
 {
-    Console.WriteLine("price with gst " + price + "$");
-    Console.WriteLine("gst difference " + Math.Round(price * 0.15m, 2) + "$");
-    Console.WriteLine("Price without gst " +  Math.Round(price * 0.85m, 2) + "$");
+    while (true) //allow the app to keep running after a calculation
+    {
+        Console.WriteLine("Enter a price to convert or type \"exit\" to close the app");
+        string input = Console.ReadLine();
+        decimal convertUserInputToDecimal;
+
+        if (input == "exit")
+        {
+            //allow user to close the terminal
+            return;
+        }
+        else if (decimal.TryParse(input, out convertUserInputToDecimal)) //detect if user input can be converted to decimal
+        {
+            decimal price = convertUserInputToDecimal; //
+            
+            Console.WriteLine("price with gst " + price + "$");
+            Console.WriteLine("gst difference " + Math.Round(price * 0.15m, 2) + "$");
+            Console.WriteLine("Price without gst " + Math.Round(price * 0.85m, 2) + "$" + Environment.NewLine);
+        }
+        else
+        {
+            Console.WriteLine("incorrect format, please enter a price in decimal");
+        }
+    }
 };
 
-calculateGst(11.50m);
+calculateGst();
 
